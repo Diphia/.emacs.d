ea432fb181518c8444d38866225ebd886a06cd66
diphia <qiyan@hey.com>
2022-04-09 11:09:30 +0800

    initial commit

---
+++ init.el
@@ -0,0 +1,40 @@
+;;; init.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Qiyan Zhang
+;;
+;; Author: Qiyan Zhang <https://github.com/diphia>
+;; Maintainer: Qiyan Zhang <qiyan@hey.com>
+;; Created: April 09, 2022
+;; Modified: April 09, 2022
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/diphia/init
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+(defconst *is-a-mac* (eq system-type 'darwin))
+
+;; Adjust garbage collection thresholds during startup, and thereafter
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq frame-title-format '("Pure Emacs\n"))
+
+
+(provide 'init)
+;;; init.el ends here
